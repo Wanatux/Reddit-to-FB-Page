e19391fff7162a193b9bf0c9c74fad84de697fee
baffometo <19743348+baffometo@users.noreply.github.com>
2021-06-29 23:57:22 -0400

    Config File
    this is the config file that will be used for the scripts that require IDs and Token, so far only contains the subreddit list and the FB and IG creds.
    
    IG Access token can be also used for FB as long as you have the right permits.

---
+++ config.py
@@ -0,0 +1,11 @@
+#Facebook
+fb_acc_token = ' ' #Enter Access Token Here
+fb_page_id = ' ' #Get Page ID Here
+
+#Instagram
+inst_acc_token = ' ' #INstagram Access Token, it can be used for FB Page as well if you have the right permissions
+inst_id = ' ' #Insta Page ID
+
+#Reddit
+subreddit_list = ['nextfuckinglevel', 'instantkarma','ActualPublicFreakouts','Whatcouldgowrong','IdiotsInCars','WinStupidPrizes', 'trashy','PublicFreakout', 'aww', 'Wellthatsucks', 'KidsAreFuckingStupid', 'therewasanattempt', 'instant_regret', 'funny', 'gaming', 'oddlysatisfying', 'wallstreetbets', 'youseeingthisshit', 'dontputyourdickinthat', 'JusticeServed', 'NoFuckingComment']
+
